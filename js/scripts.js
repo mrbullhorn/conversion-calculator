8248101cd350a022876b8bea83a6525737f02292
Hans Ellis <bullhorn@gmail.com>
2022-08-19 19:00:37 -0700

    add javascript functionality for conversion calculator

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//business logic
+function convert(faren) {
+  return (faren - 32) * 1.8;
+}
+
+
+//user interface logic
+
+const faren = prompt("Enter the temperature in farenheit");
+
+window.alert ("The temperature is " + convert(faren) + "\u00B0" + " celcius.");
